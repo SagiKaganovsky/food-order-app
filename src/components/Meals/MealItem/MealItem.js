a5a83a1857bf15e3ad7bb0db7875621dc5e119bf
Sergei Kaganovsky <sergkagan@gmail.com>
2022-04-07 15:22:57 +0300

    MealItemForm added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,14 +1,17 @@
+import MealItemForm from './MealItemForm';
 import classes from './MealItem.module.css';
 
 const MealItem = ({ id, name, description, price }) => {
     const priceFixed = `$${price.toFixed(2)}`;
-    return <li key={id} className={classes.meal}>
+    return <li className={classes.meal}>
         <div>
             <h3>{name}</h3>
             <div className={classes.description}>{description}</div>
             <div className={classes.price} >{priceFixed}</div>
         </div>
-        <div></div>
+        <div>
+            <MealItemForm id={id} />
+        </div>
     </li>
 };
 
