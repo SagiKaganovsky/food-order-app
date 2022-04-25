ee90b1ed9c77abaa5b6c7eab2134e2b1d3a4a27c
Sergei Kaganovsky <sergkagan@gmail.com>
2022-04-25 13:24:47 +0300

    remove item added

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -28,7 +28,22 @@ const cartReducer = (state, action) => {
         }
     }
     if (action.type === 'REMOVE_CART_ITEM') {
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+        const updatedTotalAmount = state.totalAmount = existingItem.price;
+        let updatedItems;
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter(item => item.id !== action.id);
+        } else {
+            const updatedItem = { ...existingItem, amount: existingItem.amount - 1 }
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
 
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
     }
     return defaultCartState
 };
@@ -48,7 +63,7 @@ const CartProvider = ({ children }) => {
         items: cartState.items,
         totalAmount: cartState.totalAmount,
         addItem: addItemToCartHandler,
-        removeItam: removeItemFromCartHandler
+        removeItem: removeItemFromCartHandler
     }
 
     return (
