db9e49ce44f466e96fd3264a9dcce682e7652e13
Sergei Kaganovsky <sergkagan@gmail.com>
2022-04-25 13:40:41 +0300

    total amount on remove fix

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -30,7 +30,7 @@ const cartReducer = (state, action) => {
     if (action.type === 'REMOVE_CART_ITEM') {
         const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
         const existingItem = state.items[existingCartItemIndex];
-        const updatedTotalAmount = state.totalAmount = existingItem.price;
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
         let updatedItems;
         if (existingItem.amount === 1) {
             updatedItems = state.items.filter(item => item.id !== action.id);
